47c5c1844fb2f03fb1a0a444a93a82ec6da6e497
Oliver ALR <oliver@Olivers-MacBook-Pro.local>
2019-01-27 22:33:28 -0600

    works

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/webhook', (req, res) => {
 
         const space = basic_hello.length();
 
-        if(received_message.text === "hola"){
+        if(received_message.text === "clave"){
           response = {
             "text":"¿Dime qué necesitas?"
           }
